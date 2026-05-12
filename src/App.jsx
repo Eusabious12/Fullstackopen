63056f690df7f7a42e2de10314e88285d90e28b8
Eusabious <Eusabious@MacBook-Air.local>
2026-05-12 19:16:39 +0300

    Exercise 2.5 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,4 @@
-const Part = ({ part }) => {
-  return <p>{part.name} {part.exercises}</p>
-}
-
-const Header = ({ course }) => {
-  return <h2>{course.name}</h2>
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(part =>
-        <Part key={part.id} part={part} />
-      )}
-    </div>
-  )
-}
-
-const Total = ({ course }) => {
-  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
-  return <p><strong>total of {total} exercises</strong></p>
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-}
+import Course from "./Course"
 
 const App = () => {
   const courses = [
