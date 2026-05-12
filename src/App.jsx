c28464d3aba661068868bc208f633e5c63e6fd37
Eusabious <Eusabious@MacBook-Air.local>
2026-05-12 17:14:28 +0300

    Exercise 2.1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,46 @@
-import { useState } from 'react'
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
 
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
+const Header = ({ course }) => {
+  return <h2>{course.name}</h2>
+}
 
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(new Array(8).fill(0))
-
-  const randomAnecdote = () => {
-    setSelected(Math.floor(Math.random() * anecdotes.length))
-  }
-
-  const vote = () => {
-    const copy = [...votes]
-    copy[selected] += 1
-    setVotes(copy)
-  }
-
-  const maxVotes = Math.max(...votes)
-  const mostVoted = votes.indexOf(maxVotes)
-
+const Content = ({ course }) => {
   return (
     <div>
-      <h2>Anecdote of the day</h2>
-      <p>{anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
-      <button onClick={vote}>vote</button>
-      <button onClick={randomAnecdote}>next anecdote</button>
+      {course.parts.map(part => 
+        <Part key={part.id} part={part} />
+      )}
+   </div>
+  )
+}
 
-      <h2>Anecdote with most votes</h2>
-      <p>{anecdotes[mostVoted]}</p>
-      <p>has {maxVotes} votes</p>
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
     </div>
   )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10, id: 1 },
+      { name: 'Using props to pass data', exercises: 7, id: 2 },
+      { name: 'State of a component', exercises: 14, id: 3 }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
