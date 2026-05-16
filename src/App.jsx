bf791a85fdeb3ad613b3bd0b735484af22105f71
Eusabious <Eusabious@MacBook-Air.local>
2026-05-16 17:51:59 +0300

    Exercise 2.11

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ filter, setFilter }) => {
   return (
@@ -33,6 +34,14 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+
+  useEffect (() => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => setPersons(response.data))
+
+
+  },[])
 
   const addPerson = (event) => {
     event.preventDefault()
